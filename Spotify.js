5c388ee08710e1229e5fc54b67b74673d258bbc1
MrVinayakGupta <163561684+MrVinayakGupta@users.noreply.github.com>
2025-10-30 02:21:43 +0530

    Update Spotify.js

--- Spotify.js
+++ Spotify.js
@@ -1,7 +1,11 @@
 async function main(){
     let a = await fetch("http://127.0.0.1:5500/Spotify/songs/");
     let response = await a.text();
-    console.log(response);
+    // console.log(response);
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a");
+    console.log(as);
 
 }
 
