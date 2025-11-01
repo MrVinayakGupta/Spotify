a7cb21b5bf12ffc941a887c95af85d5f90aae7ab
MrVinayakGupta <163561684+MrVinayakGupta@users.noreply.github.com>
2025-11-01 20:18:33 +0530

    Update Spotify.js

--- Spotify.js
+++ Spotify.js
@@ -1,54 +1,81 @@
-// async function getSongs() {
-//     let a = await fetch("http://127.0.0.1:5500/Spotify/songs/");
-//     let response = await a.text();
-//     // console.log(response);
-//     let div = document.createElement("div");
-//     div.innerHTML = response;
-//     let as = div.getElementsByTagName("a");
-//     console.log(as);
-//     let songs = [];
+async function getSongs() {
+  let a = await fetch("http://127.0.0.1:5500/Spotify/songs/");
+  let response = await a.text();
+  // console.log(response);
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
+  console.log(as);
+  let songs = [];
 
-//     for (let index = 0; index < as.length; index++) {
-//         const element = as[index];
-//         if (element.href.endsWith(".mp3")) {
-//             songs.push(element.href);
-//         }
-//     }
-//     return songs;
-// }
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href);
+    }
+  }
+  return songs;
+}
 
-// async function main() {
-//     let songs = await getSongs();
-//     console.log(songs);
-
-
-//     let play = document.getElementById("play").addEventListener("click", function () {
-//         for (let i = 0; i < songs.length; i++) {
-//             const audio = new Audio(songs[i]);
-//             audio.play();
-//             function pauseAudio() {
-//                 audio = document.getElementById('play');
-//                 audio.pause();
-//             }
-//         }
-//     });
+async function main() {
+  let songs = await getSongs();
+  console.log(songs);
 
 
-// }
+  let currentAudio = null;
+  let currentIndex = 0;
 
-// main();
+  //For Play and Pause
+  let play = document.getElementById("play").addEventListener("click", function () {
 
-
-
-const audio = document.getElementById("audioPlayer");
     const button = document.getElementById("playPauseBtn");
 
-    button.addEventListener("click", () => {
-      if (audio.paused) {
-        audio.play();
+    if (!currentAudio) {
+      // for (let i = 0; i < songs.length; i++) {
+        currentAudio = new Audio(songs[currentIndex]);
+        currentAudio.play();
         button.textContent = "Pause ⏸️";
-      } else {
-        audio.pause();
-        button.textContent = "Play ▶️";
-      }
-    });
+      // }
+    } else if (currentAudio.paused) {
+      currentAudio.play();
+      button.textContent = "Pause ⏸️";
+    } else {
+      currentAudio.pause();
+      button.textContent = "Play ▶️";
+    }
+  });
+
+  //For Next songs
+  let next = document.getElementById("nextSong").addEventListener("click", function (event) {
+    if (event.target.id === 'nextSong') {
+      currentAudio.pause();
+      currentAudio = new Audio(songs[currentIndex + 1]);
+      currentAudio.play();
+
+    }
+    return currentAudio;
+  });
+
+  //For Previes songs
+  let previes = document.getElementById("previeSongs").addEventListener("click", function (event) {
+    if(event.target.id === 'previeSongs'){
+      currentAudio.pause();
+      currentAudio = new Audio (songs[currentIndex - 1]);
+      currentAudio.play();
+    }
+  })
+
+  // for (let i = 0; i < songs.length; i++) {
+  //     const audio = new Audio(songs[i]);
+  //     audio.play();
+  //     function pauseAudio() {
+  //         audio = document.getElementById('play');
+  //         audio.pause();
+  //     }
+  // }
+  // });
+
+
+}
+
+main();
